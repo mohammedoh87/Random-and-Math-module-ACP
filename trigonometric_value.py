093509dec1b610251fe6689f1d5d5168c0b3fc69
mohammedoh87 <aamina.oh87@gmail.com>
2024-10-10 16:15:57 +0300

    Trigonometric Value

---
+++ trigonometric_value.py
@@ -0,0 +1,4 @@
+import math
+print(math.sin(int(input("Enter a number to calculate the sine: "))))
+print(math.cos(int(input("Enter a number to calculate the cosine: "))))
+print(math.tan(int(input("Enter a number to calculate the tangent: "))))
